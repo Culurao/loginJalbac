2a66b154a3068b4d41c8e6f200274cdfd5d27b27
Cbernalmonterrosa <cbernalmonterrosa@gmail.com>
2022-11-02 08:28:36 -0500

    Comit usuario actualizado

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,17 @@
-const linkRoles = () =>{
-    let log =true
+let rolIngresado = document.querySelector("#rol");
+let correoIngresado = document.querySelector("#correo");
+let contraseñaIngresado = document.querySelector("#password");
+let contraseñaIngresado2 = document.querySelector("#password2");
 
-    return new Promise((resolve, reject) =>{
-        if (log == true ){
-            resolve(location.
-            reject(alert("Nombre de usuario o contraseña invalidos"))
-        }
+const validarUsuario = () => {
+    return new Promise((resolve, reject)=>{
+      if (rolIngresado.value == '' || correoIngresado.value == '' || contraseñaIngresado.value == '' || contraseñaIngresado2.value == '') {
+        resolve (alert('Debe ingresar todos los campos'))
+      }else{
+        reject(alert('Cuenta de usuario registrada correctamente'))
+      }
     })
-    
-}
+  }
 
-const btn = document.querySelector('#buttonRoles')
-btn.addEventListener("click", linkRoles)    
+  const botonValidacion = document.querySelector("#botonUsuario");
+  botonValidacion.addEventListener("click", validarUsuario);
